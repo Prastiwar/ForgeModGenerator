8ef89a61bee360b01958384b682ac503982b9f37
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2019-01-17 15:10:33 +0100

    all texture folders are now detected and separated, added options: remove texture, add new texture, open container

--- ForgeModGenerator/app/ForgeModGenerator.UI/ViewModel/TextureGenerator/TextureGeneratorViewModel.cs
+++ ForgeModGenerator/app/ForgeModGenerator.UI/ViewModel/TextureGenerator/TextureGeneratorViewModel.cs
@@ -3,9 +3,12 @@ using ForgeModGenerator.Model;
 using ForgeModGenerator.Service;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 using System.Windows.Input;
 
@@ -24,40 +27,28 @@ namespace ForgeModGenerator.ViewModel
                 Log.InfoBox("Select mod first");
                 return;
             }
-            Blocks = FindTextures(ModPaths.TexturesBlocks(SessionContext.SelectedMod.ModInfo.Name, SessionContext.SelectedMod.ModInfo.Modid));
-            Entity = FindTextures(ModPaths.TexturesEntity(SessionContext.SelectedMod.ModInfo.Name, SessionContext.SelectedMod.ModInfo.Modid));
-            Items = FindTextures(ModPaths.TexturesItems(SessionContext.SelectedMod.ModInfo.Name, SessionContext.SelectedMod.ModInfo.Modid));
-            Armor = FindTextures(ModPaths.TexturesModelsArmor(SessionContext.SelectedMod.ModInfo.Name, SessionContext.SelectedMod.ModInfo.Modid));
+            Textures = FindAllTextures(ModPaths.Textures(SessionContext.SelectedMod.ModInfo.Name, SessionContext.SelectedMod.ModInfo.Modid));
         }
 
-        private ObservableCollection<string> blocks;
-        public ObservableCollection<string> Blocks {
-            get => blocks;
-            set => Set(ref blocks, value);
-        }
-
-        private ObservableCollection<string> entity;
-        public ObservableCollection<string> Entity {
-            get => entity;
-            set => Set(ref entity, value);
-        }
-
-        private ObservableCollection<string> items;
-        public ObservableCollection<string> Items {
-            get => items;
-            set => Set(ref items, value);
-        }
-
-        private ObservableCollection<string> armor;
-        public ObservableCollection<string> Armor {
-            get => armor;
-            set => Set(ref armor, value);
+        private ObservableCollection<TextureCollection> textures;
+        public ObservableCollection<TextureCollection> Textures {
+            get => textures;
+            set => Set(ref textures, value);
         }
 
         private ICommand addTexture;
-        public ICommand AddTexture { get => addTexture ?? (addTexture = new RelayCommand<ObservableCollection<string>>(AddNewTexture)); }
+        public ICommand AddTexture => addTexture ?? (addTexture = new RelayCommand<TextureCollection>(AddNewTexture));
 
-        private void AddNewTexture(ObservableCollection<string> collection)
+        private ICommand removeTexture;
+        public ICommand RemoveTexture => removeTexture ?? (removeTexture = new RelayCommand<Tuple<ObservableCollection<string>, string>>(RemoveTextureFrom));
+
+        private void RemoveTextureFrom(Tuple<ObservableCollection<string>, string> param)
+        {
+            param.Item1.Remove(param.Item2);
+            FileSystem.DeleteFile(param.Item2, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+        }
+
+        private void AddNewTexture(TextureCollection collection)
         {
             OpenFileDialog dialog = new OpenFileDialog() {
                 Multiselect = true,
@@ -70,21 +61,31 @@ namespace ForgeModGenerator.ViewModel
             {
                 foreach (string filePath in dialog.FileNames)
                 {
-                    // TODO: Add texture
+                    string fileName = new FileInfo(filePath).Name;
+                    string newFilePath = Path.Combine(collection.DestinationPath, fileName);
+                    File.Copy(filePath, newFilePath);
+                    collection.Textures.Add(newFilePath);
                 }
             }
         }
 
-        private ObservableCollection<string> FindTextures(string path)
+        private ObservableCollection<TextureCollection> FindAllTextures(string path)
         {
-            Mod mod = SessionContext.SelectedMod;
-            string[] texturePaths = Directory.GetFiles(path, "*.png", SearchOption.AllDirectories);
-            List<string> foundTextures = new List<string>(texturePaths.Length);
-            foreach (string texturePath in texturePaths)
+            List<TextureCollection> textures = new List<TextureCollection>();
+            foreach (string directory in Directory.EnumerateDirectories(path, "*", System.IO.SearchOption.AllDirectories))
             {
-                foundTextures.Add(texturePath);
+                bool hasAnyFile = Directory.EnumerateFiles(directory).Any();
+                if (hasAnyFile)
+                {
+                    TextureCollection texture = new TextureCollection(directory);
+                    foreach (string filePath in Directory.EnumerateFiles(directory))
+                    {
+                        texture.Textures.Add(filePath);
+                    }
+                    textures.Add(texture);
+                }
             }
-            return new ObservableCollection<string>(foundTextures);
+            return new ObservableCollection<TextureCollection>(textures);
         }
     }
 }
