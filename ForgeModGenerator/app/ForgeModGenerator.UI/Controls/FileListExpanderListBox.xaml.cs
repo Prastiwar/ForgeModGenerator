7d02b18fe071df1b196b23b8aed9e018bc50860d
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2019-01-18 11:45:05 +0100

    Show names of files in sound/texture generator

--- ForgeModGenerator/app/ForgeModGenerator.UI/Controls/FileListExpanderListBox.xaml.cs
+++ ForgeModGenerator/app/ForgeModGenerator.UI/Controls/FileListExpanderListBox.xaml.cs
@@ -10,6 +10,7 @@ namespace ForgeModGenerator.Controls
             InitializeComponent();
         }
 
+        // Default is FileListExpander.xaml
         public static readonly DependencyProperty ListTemplateProperty =
             DependencyProperty.Register("ListTemplate", typeof(DataTemplate), typeof(FileListExpanderListBox), new PropertyMetadata(Application.Current.FindResource("DefaultFileListTemplate")));
         public DataTemplate ListTemplate {
@@ -17,11 +18,12 @@ namespace ForgeModGenerator.Controls
             set => SetValue(ListTemplateProperty, value);
         }
 
-        public static readonly DependencyProperty ListItemTemplateProperty =
-            DependencyProperty.Register("ListItemTemplate", typeof(DataTemplate), typeof(FileListExpanderListBox), new PropertyMetadata(null));
-        public DataTemplate ListItemTemplate {
-            get => (DataTemplate)GetValue(ListItemTemplateProperty);
-            set => SetValue(ListItemTemplateProperty, value);
+        // FileListExpander.xaml connection with its ItemTemplate
+        public static readonly DependencyProperty FileListExpanderItemTemplateProperty =
+            DependencyProperty.Register("FileListExpanderItemTemplate", typeof(DataTemplate), typeof(FileListExpanderListBox), new PropertyMetadata(null));
+        public DataTemplate FileListExpanderItemTemplate {
+            get => (DataTemplate)GetValue(FileListExpanderItemTemplateProperty);
+            set => SetValue(FileListExpanderItemTemplateProperty, value);
         }
     }
 }
