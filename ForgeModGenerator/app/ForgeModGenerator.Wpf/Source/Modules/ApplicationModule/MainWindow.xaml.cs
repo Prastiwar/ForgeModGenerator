11121bafe9dfe965583ec9b379ce23495c95cf38
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2019-09-06 21:04:06 +0200

    Remove NavigationMenuControl, Reorganize controls folder

--- ForgeModGenerator/app/ForgeModGenerator.Wpf/Source/Modules/ApplicationModule/MainWindow.xaml.cs
+++ ForgeModGenerator/app/ForgeModGenerator.Wpf/Source/Modules/ApplicationModule/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Reflection;
+﻿using ForgeModGenerator.Components;
+using Prism.Commands;
+using System.Reflection;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ForgeModGenerator.ApplicationModule.Views
 {
@@ -9,14 +13,23 @@ namespace ForgeModGenerator.ApplicationModule.Views
         public MainWindow()
         {
             InitializeComponent();
-            NavigationMenu.InitializeMenu(ContentGrid, 0, 0);
+            InitializeMenu(ContentGrid, 0, 0);
         }
+
+        public MenuComponent MenuComponent { get; protected set; }
+        protected double MenuSlideSpeed { get; set; } = 0.25;
+        protected Vector MenuSlideOffset { get; set; } = new Vector(170, 0);
+
+        private ICommand toggleMenuCommand;
+        public ICommand ToggleMenuCommand => toggleMenuCommand ?? (toggleMenuCommand = new DelegateCommand(MenuComponent.Toggle));
 
         private MethodInfo activateFrame;
         private MethodInfo ActivateFrame => activateFrame ?? (activateFrame = FrameTransitioner.GetType().GetMethod("ActivateFrame", BindingFlags.Instance | BindingFlags.NonPublic));
 
         private readonly object[] navigateBackArgs = new object[] { -1, 0 };
         private readonly object[] navigateForwardArgs = new object[] { 0, 1 };
+
+        protected void InitializeMenu(Grid menuGrid, int row, int column) => MenuComponent = new MenuComponent(menuGrid, new MenuSettings(menuGrid, column, row, MenuSlideOffset, MenuSlideSpeed));
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
