5b5df6551f78638a1e83694063d04fe6198313c2
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2019-09-13 12:18:27 +0200

    Add filterable ItemListForm

--- ForgeModGenerator/app/ForgeModGenerator.Wpf/Source/Controls/ItemListForm.xaml.cs
+++ ForgeModGenerator/app/ForgeModGenerator.Wpf/Source/Controls/ItemListForm.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ForgeModGenerator.Core;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -15,6 +16,20 @@ namespace ForgeModGenerator.Controls
             }
         }
 
+        public static readonly DependencyProperty FilterTextProperty =
+            DependencyProperty.Register("FilterText", typeof(string), typeof(ItemListForm), new PropertyMetadata("", OnFilterChanged));
+        public string FilterText {
+            get => (string)GetValue(FilterTextProperty);
+            set => SetValue(FilterTextProperty, value);
+        }
+
+        private static void OnFilterChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            string filter = e.NewValue as string;
+            ItemListForm form = (ItemListForm)d;
+            form.SetFilter(filter);
+        }
+
         public static readonly DependencyProperty LocatorsProperty =
             DependencyProperty.Register("Locators", typeof(MCItemLocator[]), typeof(ItemListForm), new PropertyMetadata(MCItemLocator.GetAllMinecraftItems()));
         public MCItemLocator[] Locators {
@@ -27,6 +42,28 @@ namespace ForgeModGenerator.Controls
         public MCItemLocator SelectedLocator {
             get => (MCItemLocator)GetValue(SelectedLocatorProperty);
             set => SetValue(SelectedLocatorProperty, value);
+        }
+
+        private MCItemLocator[] notFilteredLocators;
+        private bool isFiltered = false;
+
+        public void SetFilter(string filter)
+        {
+            filter = filter.Trim();
+            if (string.IsNullOrEmpty(filter))
+            {
+                Locators = notFilteredLocators;
+                isFiltered = false;
+            }
+            else
+            {
+                if (!isFiltered)
+                {
+                    notFilteredLocators = Locators;
+                }
+                Locators = notFilteredLocators.Where(locator => locator.Name.Contains(filter)).ToArray();
+                isFiltered = true;
+            }
         }
 
         public void SetDataContext(object context) => DataContext = context;
