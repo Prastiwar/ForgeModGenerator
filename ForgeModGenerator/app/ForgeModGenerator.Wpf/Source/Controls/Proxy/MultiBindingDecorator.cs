c43fdeda1954ea5776f33c73589a49008481982b
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2019-12-18 15:14:35 +0100

    Fix GenericTupleConverter to create proper tuple type

---
+++ ForgeModGenerator/app/ForgeModGenerator.Wpf/Source/Controls/Proxy/MultiBindingDecorator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ForgeModGenerator.Controls
+{
+    /// <summary> MultiBinding that allows to bind ConverterParameter </summary>
+    public class MultiBindingDecorator : MarkupExtension
+    {
+        public MultiBinding MultiBinding { get; set; }
+        public Binding ConverterParameter { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            MultiBinding.Bindings.Add(ConverterParameter);
+            MultiValueConverterAdapter adapter = new MultiValueConverterAdapter {
+                Converter = MultiBinding.Converter
+            };
+            MultiBinding.Converter = adapter;
+            return MultiBinding.ProvideValue(serviceProvider);
+        }
+
+        /// <summary> Converter adapter that takes last binding as ConverterParameter </summary>
+        private class MultiValueConverterAdapter : IMultiValueConverter
+        {
+            public IMultiValueConverter Converter { get; set; }
+
+            private object lastParameter;
+
+            public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (Converter == null)
+                {
+                    return null;
+                }
+                lastParameter = values[values.Length - 1];
+                object[] nextValues = new object[values.Length - 1];
+                Array.Copy(values, 0, nextValues, 0, nextValues.Length);
+                return Converter.Convert(nextValues, targetType, lastParameter, culture);
+            }
+
+            public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            {
+                if (Converter == null)
+                {
+                    return null;
+                }
+                Type[] nextTypes = new Type[targetTypes.Length - 1];
+                Array.Copy(targetTypes, 0, nextTypes, 0, nextTypes.Length);
+                return Converter.ConvertBack(value, nextTypes, lastParameter, culture);
+            }
+        }
+    }
+}
--- ForgeModGenerator/app/ForgeModGenerator.Wpf/Source/Converters/MultiValues/GenericTupleConverter.cs
+++ ForgeModGenerator/app/ForgeModGenerator.Wpf/Source/Converters/MultiValues/GenericTupleConverter.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Prism.Commands;
+using System;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Data;
 
@@ -15,17 +17,40 @@ namespace ForgeModGenerator.Converters
             {
                 return null; // we cannot make generic type from null object
             }
-            int length = values.Length;
-            Type[] types = new Type[length];
-            for (int i = 0; i < length; i++)
+            Type[] tupleArgumentTypes = GetTupleTypes(parameter);
+            if (tupleArgumentTypes.Length != values.Length)
             {
-                types[i] = values[i].GetType();
+                return null; // values doesn't match tuple arguments we want to create
             }
-            MethodInfo methodInfo = typeof(Tuple).GetMethods().First(x => x.IsGenericMethod && x.GetGenericArguments().Length == length);
-            methodInfo = methodInfo.MakeGenericMethod(types);
+            MethodInfo methodInfo = GetCreateTupleMethod(values, tupleArgumentTypes);
             return methodInfo.Invoke(null, values);
         }
 
+        private MethodInfo GetCreateTupleMethod(object[] values, Type[] tupleArgumentTypes)
+        {
+            MethodInfo methodInfo = typeof(Tuple).GetMethods().First(x => x.IsGenericMethod && x.GetGenericArguments().Length == values.Length);
+            return methodInfo.MakeGenericMethod(tupleArgumentTypes);
+        }
+
+        private Type[] GetTupleTypes(object parameter)
+        {
+            Type[] genericTypes = parameter.GetType().GetGenericArguments();
+            bool isParameterGenericDelegateCommand = parameter != null &&
+                                                     parameter.GetType().IsGenericType &&
+                                                     genericTypes.Length == 1 &&
+                                                     typeof(DelegateCommandBase).IsAssignableFrom(parameter.GetType());
+            if (isParameterGenericDelegateCommand)
+            {
+                Type argumentType = genericTypes[0];
+                if (typeof(ITuple).IsAssignableFrom(argumentType))
+                {
+                    return argumentType.GetGenericArguments();
+                }
+            }
+            return null;
+        }
+
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException(nameof(GenericTupleConverter) + " works only one way");
+
     }
 }
